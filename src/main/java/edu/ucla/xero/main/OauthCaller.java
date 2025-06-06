6873db774e7f456f6c9f65387051252167546ec2
DRickard <drickard1967@library.ucla.edu>
2025-06-06 23:36:18 +0000

    more coding

--- src/main/java/edu/ucla/xero/main/OauthCaller.java
+++ src/main/java/edu/ucla/xero/main/OauthCaller.java
@@ -2,13 +2,18 @@ package edu.ucla.xero.main;
 
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.Form;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.client.WebTarget;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import java.lang.StringBuffer;
+
+import java.util.Base64;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
@@ -35,7 +40,7 @@ public class OauthCaller
     props = new Properties();
     try
     {
-      props.load(new FileInputStream(new File(propFile)));
+      props.load(OauthCaller.class.getClassLoader().getResourceAsStream(propFile));
     }
     catch (IOException ioe)
     {
@@ -47,10 +52,20 @@ public class OauthCaller
   private static void getRefreshToken()
   {
     Client client = ClientBuilder.newClient();
-    WebTarget target = client.target("http://localhost:9998").path("resource");
+    WebTarget target = client.target(props.getProperty("authorization_url"));
 
     Form form = new Form();
-    form.param("x", "foo");
-    form.param("y", "bar");
+    form.param("grant_type", "refresh_token");
+    form.param("refresh_token", "Zec0VuOFzLyojdcYF7KqlUNgfZyvzw67tDw2mpuGplA");
+
+    String start = "Basic ";
+    String clientID = props.getProperty("client_id");
+    String clientSecret = props.getProperty("client_secret");
+    StringBuffer buffer = new StringBuffer(clientID).append(":").append(clientSecret);
+    String encoded = Base64.getEncoder().encodeToString(buffer.toString().getBytes());
+    String authString = start.concat(encoded);
+    String jsonResp = target.request(MediaType.APPLICATION_JSON_TYPE).header("Authorization",authString).
+	    post(Entity.entity(form,MediaType.APPLICATION_FORM_URLENCODED_TYPE), String.class);
+    System.out.println(jsonResp);
   }
 }
